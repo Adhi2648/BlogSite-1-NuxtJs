591d6932dee200fb3f1b83ec56e185d8f4ae7e03
R Adhi Narayanan <69183544+Adhi2648@users.noreply.github.com>
2021-08-11 21:22:33 +0530

    Add files via upload
    Blogsite-1

---
+++ pages/about.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about-page">
+	  <h1>About Page</h1>
+  </div>
+ 
+</template>
+
+<style>
+    .about-page{
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      color: black;
+      height: 75vh;
+    }
+</style>
